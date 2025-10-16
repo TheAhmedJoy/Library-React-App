26ebed81774394f964434c1a268c370e1c8d5171
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-10-16 13:27:52 -0700

    FontAwesome Imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// FontAwesome Setup
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+library.add(faBars, faShoppingCart, faTimes);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
