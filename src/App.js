047de83a5640f688cfc83c0a168d51dcb48e67e3
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-10-17 15:36:40 -0700

    Added BooksPage to page folder, routing to BooksPage, and styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Nav from './components/Nav';
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
+import BooksPage from './pages/BooksPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={HomePage}/>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/books" component={BooksPage} />
         <Footer />
       </div>
     </Router>
