9a4e7be940db00cf2a79d1577ddf23a08706a744
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-10-20 14:07:42 -0700

    Changed landing page back to <a href/>

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import UndrawBooks from '../assets/Undraw_Books.svg'
-import { Link } from 'react-router-dom'
 
 export default function Landing() {
   return (
@@ -10,9 +9,9 @@ export default function Landing() {
                 <div className="header__description">
                     <h1>The most awarded online library platform</h1>
                     <h2>Find your dream book with <span className="purple"> Ahmed's Library</span></h2>
-                    <Link to="#features">
+                    <a href="#features">
                         <button className="btn">Browse Books</button>
-                    </Link>
+                    </a>
                 </div>
                 <figure className="header__img--wrapper">
                     <img src={UndrawBooks} alt=""></img>
