d4c72e6497ed4cffe4fd25e802bd6784cf3ca2c7
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-10-20 14:09:08 -0700

    BookInfo component

--- src/components/BookInfo.jsx
+++ src/components/BookInfo.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 import React from 'react'
+import Rating from './ui/Rating'
+import Price from './ui/Price'
 
 export default function BookInfo({ books }) {
     return (
@@ -12,11 +14,21 @@ export default function BookInfo({ books }) {
                             <Link to="/books" className="book__link">
                                 <FontAwesomeIcon icon="arrow-left" />
                             </Link>
-                            <Link to="/book">
-                                <h2 className="book__selected--title--top">
-
-                                </h2>
+                            <Link to="/books" className="book__link">
+                                <h2 className="book__selected--title--top">Books</h2>
                             </Link>
+                        </div>
+                        <div className="book__selected">
+                            <figure className="book__selected--figure">
+                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                            </figure>
+                            <div className="book__selected--description">
+                                <h2 className="book__selected--title">Cracking the Coding Interview</h2>
+                                <Rating rating="4.5"/>
+                                <div className="book__selected--price">
+                                    <Price originalPrice={50} salePrice={20} />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
