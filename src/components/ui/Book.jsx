ed7f168d0a8aa9ae112433fd536695402d998a6b
TheAhmedJoy <TheAhmedJoyAli@gmail.com>
2025-10-20 18:12:48 -0700

    Added skeleton loading states to book items on pages

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Rating from './Rating'
-import Price from './Price'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Price from "./Price";
 
 export default function Book({ book }) {
-    return (
-        <div className="book">
-            <Link to={`/books/${book.id}`}>
-                <figure className="book__img--wrapper">
-                    <img src={book.url} alt={book.title + " Image"} className="book__img" />
-                </figure>
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        if (mountedRef.current) {
+          setImg(image);
+        }
+      }, 300);
+    };
+    return () => {
+      mountedRef.current = false;
+    };
+  }, [book.url]);
+
+  return (
+    <div className="book">
+      {!img ? (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      ) : (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img className="book__img" src={img.src} alt="" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
             </Link>
-            <div className="title">
-                <Link to={`/books/${book.id}`} className="book__title--link">
-                    {book.title}
-                </Link>
-            </div>
-            <Rating rating={book.rating} />
-            <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
-        </div>
-    )
-}
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />
+        </>
+      )}
+    </div>
+  );
+};
